785f8016dc3a605f2781d46e13c4117f07bf914b
Bryson Longoria <bryson326@gmail.com>
2021-02-21 11:18:34 -0500

    create angular controller and edit web route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AngularController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::any('/{any}', [AngularController::class, 'index'])->where('any', '^(?!api).*$');
